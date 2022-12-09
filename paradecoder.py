44147f7dd5b43eda848c2a8d54779349c9fe702f
markusthilo <markus.thilo@gmail.com>
2022-12-09 13:34:35 +0100

    Updated logging and README

--- paradecoder.py
+++ paradecoder.py
@@ -6,9 +6,21 @@ __version__ = '0.1_2022-12-09'
 __license__ = 'GPL-3'
 __email__ = 'markus.thilo@gmail.com'
 __status__ = 'Testing'
-__description__ = 'Versatile decoder with openssl in mind'
+__description__ = '''ParaDecoder creates command lines by combining lists
+and executing the commands in parallel threads. The initial task was to test
+openssl decryption. Other taks/applications are possible. The Files have to
+contain one argument per line, e.g. ciphers or passwords. 
+'''
+__example__ = '''Example:
+$ python paradecoder.py -t 4 -v 'openssl {ciphers.txt} -d -pbkdf2
+-in sample_.enc -k {pw_list.txt}'
+'''
 
-import logging
+from logging import basicConfig as logConfig
+from logging import INFO as logINFO
+from logging import DEBUG as logDEBUG
+from logging import info as log_info
+from logging import debug as log_debug
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import Popen, PIPE
@@ -18,12 +30,12 @@ from sys import exit as sysexit
 class Logger:
 	'Logging for this tool'
 
-	def __init__(self, level=logging.ERROR, filename=None):
+	def __init__(self, level=logINFO, filename=None):
 		'Initiate logging by given level and to given file'
 		if filename == None:
-			logging.basicConfig(level=level, format = '%(message)s')
+			logConfig(level=level, format = '%(message)s')
 		else:
-			logging.basicConfig(
+			logConfig(
 				level=level,
 				filename = filename,
 				format = '%(asctime)s %(levelname)s: %(message)s',
@@ -32,7 +44,7 @@ class Logger:
 			logging.info(f'Start logging to {filename}')
 
 class Exec(Popen):
-	'Command to execute'
+	'Command to execute, use Popen from subprocess'
 
 	def __init__(self, cmd):
 		'Start running command'
@@ -74,8 +86,6 @@ class GenCmd:
 class Threads:
 	'Thread handler'
 
-	ENCODING = 'utf-8'	# to decode stdout and stderr
-
 	def __init__(self, cmdgen, maxthreads):
 		'Generate list with process handlers'
 		self.cmdgen = cmdgen
@@ -91,23 +101,22 @@ class Threads:
 			try:
 				cmd = next(self.cmdgen)
 			except StopIteration:
-				return None
-			logging.debug(f'Threads: trying to execute as thread {len(self.procs)}: {cmd}')
+				return
+			log_debug(f'Threads: trying to execute as thread {len(self.procs)}: {cmd}')
 			self.procs.append(Exec(cmd))
 			return len(self.procs)
 
 	def check(self):
 		'Check for finished processes and give return code'
 		for proc in self.procs:
-			returncode = proc.poll()
-			stdout, stderr = proc.communicate()
-			if returncode != None:
+			if proc.poll() != None:
 				self.procs.remove(proc)
-				return returncode, stdout.decode(self.ENCODING), stderr.decode(self.ENCODING) 
-		return None, None, None
+				return proc
 
 class Brake:
 	'Criteria to end app'
+
+	ENCODING = 'utf-8'	# to decode stdout and stderr
 
 	def __init__(self, brake):
 		'Chose brake'
@@ -115,9 +124,18 @@ class Brake:
 			'0': self.__exit0__
 		}[brake]
 
-	def __exit0__(self, returncode, stdout, stderr):
+	def dec(self, encoded):
+		'Try to decode STDOUT or STDERR'
+		try:
+			return encoded.decode(self.ENCODING) 
+		except:
+			return None
+
+	def __exit0__(self, proc):
 		'Process returned 0?'
-		return returncode == 0
+		returncode = proc.poll()
+		stdout, stderr = proc.communicate()
+		return returncode == 0, proc.args, returncode, self.dec(stdout), self.dec(stderr)
 
 class Worker:
 	'Main class'
@@ -126,31 +144,35 @@ class Worker:
 
 	def __init__(self, cmd, brake, maxthreads):
 		'The work is done here'
-		logging.debug('Starting Worker')
+		log_debug('Starting Worker')
 		self.brake = Brake(brake)
 		self.threads = Threads(GenCmd(cmd).get(), maxthreads)
 
 	def loop(self):
 		'Main loop, returns 0 on brake/success'
 		while len(self.threads.procs) > 0:
-			returncode, stdout, stderr = self.threads.check()
-			print('Worker:', returncode, stdout, stderr)
-			if returncode == None:
+			proc = self.threads.check()
+			if proc == None:
 				sleep(self.SLEEP)
 				continue
-			if self.brake.check(returncode, stdout, stderr):
-				logging.info(
-					f'Worker: Brake, return code: {returncode}\nSTDOUT:\n{stdout}\nSTDERR:\n{stderr}'
+			stdout, stderr = proc.communicate()
+			interrupt, args, returncode, outstr, errstr = self.brake.check(proc)
+			if interrupt:
+				log_debug('Worker: Brake returned True')
+				log_info(
+					'The following command matched the brake criteria:\n' + ' '.join(args)
+					+ f'\nReturn Code: {returncode}\nSTDOUT:\n{outstr}\nSTDERR:\n{errstr}'
 				)
 				return 0
 			self.threads.add()
-		logging.info('Worker finished: tried all combinations.')
+		log_debug('Worker finished: Tried all combinations')
+		log_info('Nothing matched.')
 		return 1
 
 if __name__ == '__main__':	# start here if called as application
-	argparser = ArgumentParser(description=__description__)
+	argparser = ArgumentParser(description=__description__, epilog=__example__)
 	argparser.add_argument('-b', '--brake', type=str,  default='0',
-		help='Brake on: 0 = exit code is 0 (default)', metavar='STRING'
+		help='Exit on: 0 = exit code is 0 (default)', metavar='STRING'
 	)
 	argparser.add_argument('-l', '--logfile', type=Path,
 		help='Set logfile', metavar='FILE'
@@ -162,13 +184,13 @@ if __name__ == '__main__':	# start here if called as application
 		help='Set loglevel debug'
 	)
 	argparser.add_argument('cmd', type=str,  nargs=1,
-		help='Command to execute', metavar='STRING'
+		help='Command to execute, give replacement files in {}', metavar='STRING'
 	)
 	args = argparser.parse_args()
 	if args.verbose:
-		loglevel = logging.DEBUG
+		loglevel = logDEBUG
 	else:
-		loglevel = logging.ERROR
+		loglevel = logINFO
 	Logger(loglevel, args.logfile)
 	worker = Worker(args.cmd[0], args.brake, args.threads)
 	sysexit(worker.loop())
